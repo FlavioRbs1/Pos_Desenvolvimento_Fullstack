8334be3cd98c982e78c688a5020197344481a80c
Flavio <flaviorbs3@gmail.com>
2023-07-16 17:23:15 -0300

    Second Commit

---
+++ Javascript_curso/Strings/strings1.js
@@ -0,0 +1,21 @@
+/* Métodos mais conhecidos String
+
+substr()
+substring()
+replace()
+search()
+toLocaleLowerCase()
+toLocaleUpperCase()
+indexOf()
+includes()
+slice()
+concat()
+lastIndexOf()
+charCodeAt()
+trim()
+chartAt()
+valueOf()
+split()
+toString()
+match()
+*/
